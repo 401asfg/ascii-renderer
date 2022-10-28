9da2e451c3e7e81dfd4e8a4ffe54b00de75c946e
401asfg <mb.allan@ymail.com>
2022-10-27 23:50:41 -0700

    Improved the setup.py's description of the package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='ascii_renderer',
       version='1.0.0',
-      description='A toolset for rendering dynamic scenes as ascii characters',
+      description='A toolset for rendering a dynamic scene as a series of ascii characters',
       author='Michael Allan',
       packages=['ascii_renderer'],
       zip_safe=False)
